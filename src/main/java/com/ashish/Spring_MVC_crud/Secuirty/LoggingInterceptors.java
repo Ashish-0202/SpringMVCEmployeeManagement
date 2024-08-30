2017c3e409cc8cdb3b2147db83c3509e0cbc531b
Ashish-0202 <ashishshenoy81@gmail.com>
2024-08-30 21:42:33 +0530

    Added HandlerInterceptor to control the incoming requests as well as to control the outgoing response

---
+++ src/main/java/com/ashish/Spring_MVC_crud/Secuirty/LoggingInterceptors.java
@@ -0,0 +1,26 @@
+package com.ashish.Spring_MVC_crud.Secuirty;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class LoggingInterceptors implements HandlerInterceptor {
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println(request.getAuthType()+ " "+ request.getRequestURI()+ " "+ request.getMethod());
+        return true;
+    }
+}
